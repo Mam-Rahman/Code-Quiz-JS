d9187e1683251de9a95357c764117ce30dff53bc
Mamunur Rahman <mamunur@mac-air.home>
2023-09-05 22:33:06 +0100

    function code and timer added to logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -18,6 +18,68 @@ let sfxRight = new Audio("assets/sfx/correct.wav");
 
 
 
+function getQuestion(){
+
+}
+
+function questionCLick() {
+
+}
+
+function quizEnd(){
+    clearInterval(timerID);
+
+    let endScreenElement = document.getElementById("end-screen");
+    endScreenElement.removeAttribute("class");
+
+
+    let finalScoreElement = document.getElementById("final-score");
+    finalScoreElement.textContent = time;
+
+    questionsElement.setAttribute("class", "hide");
+}
+
+function clockTick(){
+    time--;
+    timerElement.textContent = time;
+
+    if(time <= 0){
+        quizEnd();
+    }
+}
+
+function startQuiz(){
+    let startScreenElement = document.getElementById("start-screen");
+    startScreenElement.setAttribute("class", "hide");    
+
+
+    questionsElement.removeAttribute("class");
+
+
+    timerID = setInterval(clockTick, 1000)
+
+    timerElement.textContent = time;
+
+    getQuestion();
+}
 
 
 
+
+
+
+
+function saveHighScore(){
+
+}
+
+function checkForEnter(event){
+
+}
+
+
+startButton.addEventListener("click", startQuiz);
+
+submitButton.addEventListener("click", saveHighScore);
+
+initialElement.addEventListener("keyup", checkForEnter);
