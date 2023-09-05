82c27da065c07c17af5290d1bd3e86a367c535d8
Mamunur Rahman <mamunur@mac-air.home>
2023-09-05 19:25:01 +0100

    questions added to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+let questions = [
+   {
+    "title": "Which planet is named after the Roman god of war?",
+    "choices": ["Mercury", "Venus", "Mars", "Saturn"],
+    "answer": "Mars"
+   },
+   {
+    "title": "Which planet is closest to the sun?",
+    "choices": ["Jupiter", "Earth", "Neptune", "Mercury"],
+    "answer": "Mercury"
+   },
+   {
+    "title": "What planet is third from the sun?",
+    "choices": ["Moon", "Mars", "Earth", "Saturn"],
+    "answer": "Earth"
+   },  
+]
