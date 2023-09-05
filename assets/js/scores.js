93ba3300e65769ed421e4ec62e99995ff9d5637f
Mamunur Rahman <mamunur@mac-air.home>
2023-09-05 17:42:01 +0100

    print and clear high scores function code added in scores js

---
+++ assets/js/scores.js
@@ -0,0 +1,16 @@
+function printHighScores(){
+    // TODO
+}
+
+function clearHighScores(){
+    // TODO
+}
+
+
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearHighScores);
+
+
+// document.getElementById("clear").onClick = clearHighScores;
+
+printHighScores();
